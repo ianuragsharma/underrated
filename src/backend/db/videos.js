c39528681616eef11a98397a84fb78c23160d7c1
Anurag Sharma <anuragsharma0711@gmail.com>
2022-05-18 17:53:10 +0530

    chore : added video to db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -2,20 +2,146 @@
  * Videos Database can be added here.
  * You can add videos of your wish with different attributes
  * */
-
+//  Indie Songs
+// Hip-Hop
+// Pop
+// Bollywood
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "fWBBjAeIZms",
+    title:
+      "Kammo Ji - 60's Bollywood Lo-fi | Recovering Pizza Addict | Hashparker Music",
+    description: "Song Credits - Harsh Choudhary A.K.A. hashparkermusic    ",
+    creator: "Recovering Pizza Addict",
+    views: "24K",
+    dateUploaded: "2 years ago",
+    runTime: "2:49",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/fWBBjAeIZms/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/OxFeKZ3gLeAASmR7uiv39-LTA8ShglraaYIGXscVNA03klS1RGesTtyp3IbwFv6gGmxhwoXc=s176-c-k-c0x00ffffff-no-rj",
+    category: "Indie Songs",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "UfvQjcu0c48",
+    title: "Umer Farooq - Teri Ay (Official Music Video)",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Umer Farooq - Teri Ay Album: Patang  Written, composed and performed by Umer FarooqProduced by Saad Sultan Directed by Raza Shah",
+    creator: "Umer Farooq",
+    views: "1.2M",
+    dateUploaded: "3 Years ago",
+    runTime: "3:55",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/UfvQjcu0c48/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/nVdRF87ZA8-16G8okx6zPGR8IGOm0NfQiij7IrkidsRScvHTW8Wf4hYSsKVg0C_lbBHSJNtV=s176-c-k-c0x00ffffff-no-rj",
+    category: "Indie Songs",
+  },
+  {
+    _id: "WL-3zvnUomk",
+    title: "Crew Love",
+    description:
+      " Provided to YouTube by Universal Music Group Crew Love · Drake · The Weeknd Take Care",
+    creator: "Drake",
+    views: "15M",
+    dateUploaded: "10 year ago",
+    runTime: "3:29",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/WL-3zvnUomk/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLRRnYQvFT7xYnEOtOhnEcDyheW8R5n6nTvRN3Nrvg=s900-c-k-c0x00ffffff-no-rj",
+    category: "Hip-Hop",
+  },
+  {
+    _id: "nDHFCZBM-lM",
+    title:
+      "Hey Ya ! Full Video Song | Karthik Calling Karthik | Farhan Akhtar, Deepika Padukone",
+    description:
+      "Presenting the full video song Hey Ya!  from the Bollywood movie 'Karthik Calling Karthik' featuring Farhan Akhtar, Deepika Padukone in the lead role. The song is sung by Loy Mendonca, Shankar Mahadevan, Clinton Cerejo, and the music is composed by Shankar Mahadevan, Ehsaan Noorani, Loy Mendonca. The lyrics are written by Javed Akhtar.",
+    creator: "T-Series",
+    views: "230K",
+    dateUploaded: "10 year ago",
+    runTime: "2:03",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/nDHFCZBM-lM/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/DzaZaTX6gdgjjPI_vkNc2dPbI794UroI9fTAunua0fa7lukDj5NDkjfhS5-w2KXuvXS02r92=s900-c-k-c0x00ffffff-no-rj",
+    category: "Bollywood",
+  },
+  {
+    _id: "1sLB79ElB40",
+    title: "Abhin & Tanish - Kami Hai | Official Lyric Video",
+    description:
+      "This track revolves around the idea of contemplating reasons behind the absence of something/ somebody, understanding our contributing faults, facing our never departing regrets and embracing the permanent voids that are left behind.",
+    creator: "Abhin & Tanish",
+    views: "27k",
+    dateUploaded: "1 year ago",
+    runTime: "4:17",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/1sLB79ElB40/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLTstlj_L30bwLQ6A7NwCeYZdRtOJqP0Cu7MBP3M1Q=s900-c-k-c0x00ffffff-no-rj",
+    category: "Indie Songs",
+  },
+  {
+    _id: "T1iIIZtP_eE",
+    title: "gigi - was it pog?",
+    description:
+      "SONG MADE BY king's scraps  https://soundcloud.com/boitheking/was... !! If you enjoyed the video be sure to 👍 Like and Subscribe! And turn on 🔔 Bell Notifications for when I post new videos! ",
+    creator: "Pokelawls",
+    views: "279k",
+    dateUploaded: "1 year ago",
+    runTime: "1:15",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/klZarL9tk9c/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLSgEOY2JmxgUDnUxkmNYl4oehi2y0_51GJR_ZhC3g=s176-c-k-c0x00ffffff-no-rj",
+    category: "Pop",
+  },
+  {
+    _id: "Oge-Eaik0qY",
+    title: "Raghav Meattle - City Life | Official Music Video",
+    description:
+      "City Life tries to depict life in Bombay - the hustle, bustle, grime and dust of the day, in contrast to the calm and emptiness of night. In its own way, the film is a tribute to Bombay, which thousands flock to everyday, like a moth to a bulb, unable to move itself away. The film was processed and developed at Andec Filmtechnik, Berlin.",
+    creator: "Raghav Meattle",
+    views: "333K",
+    dateUploaded: "2 year ago",
+    runTime: "2:57",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/Oge-Eaik0qY/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLTi6sxEpGZEQBWkZnEWKi2KQYMRrJewPI9KRA6onSA=s176-c-k-c0x00ffffff-no-rj",
+    category: "Indie Songs",
+  },
+  {
+    _id: "cqD2NWJuJO0",
+    title: "Depression Jam - The Outro By The Voidz (Cover)",
+    description:
+      "I was feeling a bit down but then I started jamming and ended up feeling better -- nothing a good ol two five one four chord prog cant do for ya!!especially during these weird times of quarantine blues :P",
+    creator: "Andres Cisneros",
+    views: "63K",
+    dateUploaded: "2 year ago",
+    runTime: "4:54",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/cqD2NWJuJO0/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLQTTRj9JMWUDVQKTAjseeT3bNlOv2pr0RNHOvgS8Q=s176-c-k-c0x00ffffff-no-rj",
+    category: "Pop",
+  },
+
+  {
+    _id: "snXlkWuaOvI",
+    title: "ASHWYN & Tyler Simmons Five Miles",
+    description:
+      "My new track with Tyler Simmons is finally out!! I had a blast making it and hope you guys enjoy this one!! Shoutout to Aaron Wolff for the awesome flute part!!",
+    creator: "ASHWYN",
+    views: "4K",
+    dateUploaded: "2 years ago",
+    runTime: "2:37",
+    alt: "song",
+    tumbnailURL: "https://img.youtube.com/vi/snXlkWuaOvI/maxresdefault.jpg",
+    avatar:
+      "https://yt3.ggpht.com/xoaD3J3hwdxNXrfE_Vv42F6lrKJ7BR3Si2aBfIdwaINxg8gIuk5mvEJ3-zuVQ4E7xCMY6OIKiw=s176-c-k-c0x00ffffff-no-rj",
+    category: "Indie Songs",
   },
 ];
