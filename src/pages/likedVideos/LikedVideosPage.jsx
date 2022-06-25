ac8bf295d0ffd474aff131146c6a62e5debaf426
Anurag Sharma <anuragsharma0711@gmail.com>
2022-06-25 18:10:57 +0530

    feat:added heading to liked video page

--- src/pages/likedVideos/LikedVideosPage.jsx
+++ src/pages/likedVideos/LikedVideosPage.jsx
@@ -7,11 +7,14 @@ const LikedVideosPage = () => {
   const { videoState, videoDispatch } = useVideo();
   const { liked } = videoState;
   const likedList = liked.map((video) => <ActionVideoCard video={video} />);
-
+  console.log(liked.length);
   return (
     <div>
       <Navbar />
       <div className="continer">
+        <h5 className="text-center fw-500 my-7">
+          Liked Videos ({liked.length})
+        </h5>
         <div className="flex-row  ml-1 videos-container"> {likedList}</div>
       </div>
     </div>
