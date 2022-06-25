68a77dd18d59a4e2960fc6bee5b28a9042ca6487
Anurag Sharma <anuragsharma0711@gmail.com>
2022-06-25 18:12:37 +0530

    feat: added profile page

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -3,10 +3,11 @@ import { useNavigate } from "react-router-dom";
 import { Navbar } from "../../components";
 import { useAuth } from "../../context";
 import { useDocumentTitle } from "../../hooks";
-
+import "./profilePage.css";
 const ProfilePage = () => {
   useDocumentTitle("Playlist");
-  const { setUser } = useAuth();
+  const { user, setUser } = useAuth();
+  const { firstName, lastName, email } = user;
   const navigate = useNavigate();
   const logoutHandler = () => {
     setUser(null);
@@ -17,13 +18,19 @@ const ProfilePage = () => {
     <div>
       <Navbar />
       <div className="continer">
-        <button
-          className="btn btn-solid-primary logout-btn text-base text-white"
-          onClick={logoutHandler}
-        >
-          Logout
-        </button>
-        This is Profile page
+        <div className="profile-conatiner flex-row justify-center">
+          <div className="profile-info p-4">
+            <h5 className="fw-500">Profile Info</h5>
+            <p className="my-1">Name : {firstName + " " + lastName}</p>
+            <p className="my-1">Email : {email} </p>
+            <button
+              className="btn btn-solid-primary logout-btn text-base text-white"
+              onClick={logoutHandler}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
